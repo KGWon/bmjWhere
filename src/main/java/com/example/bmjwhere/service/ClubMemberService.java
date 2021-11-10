b9c5f1218fca1a14ddd642460384a91033236279
KGWon <93180301+KGWon@users.noreply.github.com>
2021-11-10 16:07:25 +0900

    Update ClubMemberService.java

--- src/main/java/com/example/bmjwhere/service/ClubMemberService.java
+++ src/main/java/com/example/bmjwhere/service/ClubMemberService.java
@@ -13,12 +13,10 @@ import java.util.Map;
 public interface ClubMemberService  {
 
 
-    String register2(ClubMemberDTO clubMemberDTO); // 영화 등록
-    // Map<>: Movie 객체와 MovieImage 객체 2개의 객체를 Object 타입으로 Map 컬렉션에 담아서 반환
-    default Map<String, Object> dtoToEntity(ClubMemberDTO clubMemberDTO) { // map 컬렉션 타입으로 반환
+    String register2(ClubMemberDTO clubMemberDTO);  
+    default Map<String, Object> dtoToEntity(ClubMemberDTO clubMemberDTO) {  
         Map<String, Object> entityMap = new HashMap<>();
-
-        // MovieDTO 객체를 Movie Entity 타입으로 변환
+ 
         ClubMember clubMember = ClubMember.builder()
                 .email(clubMemberDTO.getEmail())
                 .name(clubMemberDTO.getName())
@@ -29,12 +27,12 @@ public interface ClubMemberService  {
 
         clubMember.addMemberRole(ClubMemberRole.USER);
 
-        entityMap.put("clubMember", clubMember); // Map 컬렉션에 추가
+        entityMap.put("clubMember", clubMember);  
         return entityMap;
     }
 
     default ClubMemberDTO entitiesToDTO(ClubMember clubMember) {
-        // Movie Entity -> MovieDTO, 영화 객체
+      
         ClubMemberDTO clubMemberDTO = ClubMemberDTO.builder()
                 .email(clubMember.getEmail())
                 .name(clubMember.getName())
