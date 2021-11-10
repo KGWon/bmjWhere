fb630bd681bcbde142fde06803f16b9b0eda5048
KGWon <93180301+KGWon@users.noreply.github.com>
2021-11-10 16:24:00 +0900

    Update ResturantController.java

--- src/main/java/com/example/bmjwhere/controller/ResturantController.java
+++ src/main/java/com/example/bmjwhere/controller/ResturantController.java
@@ -95,7 +95,7 @@ public class ResturantController {
 
     }
 
-    @PostMapping("/remove") // 영화 삭제 기능
+    @PostMapping("/remove")  
     public String remove(long rno, RedirectAttributes redirectAttributes) {
         log.info("rno: " + rno);
         resturantService.removeWithReplise(rno);
@@ -103,7 +103,7 @@ public class ResturantController {
         return "redirect:/resturant/list";
     }
 
-    @PostMapping("/modify") // 영화 수정 기능
+    @PostMapping("/modify")  
     public String modify(ResturantDTO dto, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, RedirectAttributes redirectAttributes) {
         log.info("post modify..................");
         log.info("dto: " + dto);
@@ -171,10 +171,9 @@ public class ResturantController {
     @PostMapping("/register2")
     public String register2(ClubMemberDTO clubMemberDTO, RedirectAttributes redirectAttributes) {
         log.info("clubmemberDTO: " + clubMemberDTO);
-
-        // 영화 객체와 영화 이미지 List<> 객체를 DB에 저장 후 영화 번호만 가져옴
+ 
         String email = ClubMemberService.register2(clubMemberDTO);
-//
+ 
         // addAttribute로 전달한 값은 url뒤에 붙으며, 리프레시(새로고침)해도 데이터가 유지
         // addFlashAttribute로 전달한 값은 url뒤에 붙지 않는다. 일회성이라 리프레시할 경우 데이터가 소멸한다.
         // 또한 2개이상 쓸 경우, 데이터는 소멸한다. 따라서 맵을 이용하여 한번에 값을 전달해야한다.
